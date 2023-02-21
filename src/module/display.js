e9fe294ff669d4038cd86999dddc5c3405caf125
Kagheni <Kaghenimbale@gmail.com>
2023-02-21 17:48:44 +0200

    Add the array of object in displa function

---
+++ src/module/display.js
@@ -0,0 +1,30 @@
+const renderToDo = () => {
+  const listItems = document.getElementById('list-items');
+
+  const newData = [
+    {
+      description: 'Wash Car',
+      completed: false,
+      index: 0,
+    },
+
+    {
+      description: 'Clean Clothes',
+      completed: false,
+      index: 1,
+    },
+
+    {
+      description: 'Running',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'Travel',
+      completed: false,
+      index: 2,
+    },
+  ];
+};
+
+export default renderToDo;
